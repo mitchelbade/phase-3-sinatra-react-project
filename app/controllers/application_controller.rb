7bb26c76c10ae42360c9a23ca80e57dc20636cd9
mitchelbade <mitchelm.bade@gmail.com>
2022-11-06 20:23:32 -0800

    manufacturers finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,7 @@ class ApplicationController < Sinatra::Base
 
   get '/vehicle_models' do
     vehicle_models = VehicleModel.all 
-    vehicle_models.to_json 
+    vehicle_models.to_json
   end
 
   post '/vehicle_models/new' do 
